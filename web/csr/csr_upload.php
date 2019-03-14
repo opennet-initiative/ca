317af5481326bdb8c07f126655f4cdb7e38d99dd
Mathias Mahnke <ap27@opennet-initiative.de>
2019-03-14 10:27:23 +0100

    csr upload, separate config and code

--- web/csr/csr_upload.php
+++ web/csr/csr_upload.php
@@ -1,24 +1,5 @@
 <?php
-// config variables
-$debug = false;
-//$debug = true;
-$allowedExts = array("csr");
-$allowedType = array(
-  "application/octet-stream", 
-  "application/pkcs10",
-  "text/opennet_csr");
-$uploadFolder = "/var/www/opennetca_upload";
-$cnFilter = array(
-  "aps.on" => "vpn-user",
-  "mobile.on" => "vpn-user",
-  "ugw.on" => "vpn-ugw",
-  "client.on" => "client",
-  "opennet-initiative.de" => "server");
-$mailto = "Opennet CSR Team <csr@opennet-initiative.de>";
-$mailfrom = "Opennet CA <opennetca@opennet-initiative.de>";
-$mailsubject = "Opennet CA (upload): Signing Request / Zertifikatsanfrage";
-$mailfooter = "-- \r\nOpennet Initiative e.V.\r\nhttp://www.opennet-initiative.de\r\nCA Status: http://ca.opennet-initiative.de";
-$approveurl = "https://ca.opennet-initiative.de/internal/csr_approve.php?";
+require_once("csr_upload_config.php");
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
